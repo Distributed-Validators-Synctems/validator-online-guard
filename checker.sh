86e8c4fe9ce06f4f9df8a663a373ffb09d9a486a
Albert Andrejev <albert.andrejev@gmail.com>
2022-03-14 21:00:19 +0200

    fix variable type

--- checker.sh
+++ checker.sh
@@ -26,7 +26,7 @@ PORT_ACCESS=$(nc -zvw1 ${NODE_ADDRESS} ${P2P_PORT} > /dev/null 2>&1 ; echo $?)
 if [ "$PORT_ACCESS" -eq "0" ]; then
     eval "${KILL_COMMAND}"
 
-    local MESSAGE=$(cat <<-EOF
+    MESSAGE=$(cat <<-EOF
 <b>[Success] Chain node was stopped</b>
 
 Hostname: <b>$(hostname)</b>
